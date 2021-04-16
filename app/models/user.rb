4cbfe42deca2695faf8fd11a6aead6c1fc08fcf9
RailonA <railonacosta@gmail.com>
2021-04-16 12:34:43 -0400

    Add Friendship Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,44 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   validates :name, presence: true, length: { maximum: 20 }
-
+  validates_uniqueness_of :name
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :friendships_confirmation, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :friendships_confirmation
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+  has_many :inverse_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friend_requests, through: :inverse_friendships
+
+  def pending_friends
+    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
+  end
+
+  def confirm_friend(user)
+    person = inverse_friendships.find { |friendship| friendship.user == user }
+    person.confirmed = true
+    person.save
+    Friendship.create!(user_id: id, friend_id: user.id, confirmed: true)
+  end
+
+  def reject_friendship(user)
+    friendship = inverse_friendships.find { |f| f.user == user }
+    friendship.destroy
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
+  def friends_and_own_posts
+    Post.where(user: (friends.to_a << self)).sort_by { |post| post[:created_at] }.reverse
+  end
 end
